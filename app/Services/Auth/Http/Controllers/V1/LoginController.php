5fe38aef89f4bf4328a8d0e207a3ddafd640973c
Alireza Mousavi <mousaviali1381@gmail.com>
2023-10-09 12:38:27 +0330

    + complete Auth service documentation

--- app/Services/Auth/Http/Controllers/V1/LoginController.php
+++ app/Services/Auth/Http/Controllers/V1/LoginController.php
@@ -6,7 +6,30 @@ use App\Domains\Auth\Requests\LoginRequest;
 use App\Services\Auth\Features\LoginFeature;
 use Illuminate\Http\Response;
 use Lucid\Units\Controller;
+use OpenApi\Annotations as OA;
 
+/**
+ * @OA\Post(
+ *     path="/api/v1/login",
+ *     tags={"auth"},
+ *     summary="Login user with email and password",
+ *     @OA\RequestBody(
+ *         required=true,
+ *         @OA\JsonContent(
+ *             @OA\Property(property="email", type="string", format="email", example="example@gmail.com"),
+ *             @OA\Property(property="password", type="string", format="password", description="Must be greater than 6", example="password")
+ *         )
+ *     ),
+ *     @OA\Response(
+ *         response=200,
+ *         description="Successful login"
+ *     ),
+ *     @OA\Response(
+ *         response=422,
+ *         description="Unprocessable - Invalid credentials"
+ *     )
+ * )
+ */
 class LoginController extends Controller
 {
     public function __construct(
