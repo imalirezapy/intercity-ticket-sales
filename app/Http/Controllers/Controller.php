0d66f93bd98c795f1de4f80fe4b4c5835275e5c7
Alireza Mousavi <mousaviali1381@gmail.com>
2023-10-09 12:37:56 +0330

    + install l5-swagger

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -5,7 +5,14 @@ namespace App\Http\Controllers;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use OpenApi\Annotations as OA;
 
+/**
+ * @OA\Info(
+ *     title="intercity ticket sales restful api",
+ *     version="1.0"
+ * )
+ */
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
