ed64798c9f0300000dcc60cfcffd9d2bcc308cad
Alireza Mousavi <mousaviali1381@gmail.com>
2023-10-09 12:38:27 +0330

    + complete Auth service documentation

--- app/Services/Auth/Http/Controllers/V1/LoginController.php
+++ app/Services/Auth/Http/Controllers/V1/LoginController.php
@@ -6,7 +6,30 @@ use App\Domains\Auth\Requests\LoginRequest;
 use App\Services\Auth\Features\LoginFeature;
 use Illuminate\Http\Response;
 use Lucid\Units\Controller;
+use OpenApi\Annotations as OA;
 
+/**
+ * @OA\Post(
+ *     path="/login",
+ *     tags={"auth"},
+ *     summary="Login user with email and password",
+ *     @OA\RequestBody(
+ *         required=true,
+ *         @OA\JsonContent(
+ *             @OA\Property(property="email", type="string", format="email", description="User's email"),
+ *             @OA\Property(property="password", type="string", format="password", description="User's password")
+ *         )
+ *     ),
+ *     @OA\Response(
+ *         response=200,
+ *         description="Successful login"
+ *     ),
+ *     @OA\Response(
+ *         response=422,
+ *         description="Unprocessable - Invalid credentials"
+ *     )
+ * )
+ */
 class LoginController extends Controller
 {
     public function __construct(
--- app/Services/Auth/Http/Controllers/V1/RegisterController.php
+++ app/Services/Auth/Http/Controllers/V1/RegisterController.php
@@ -6,7 +6,33 @@ use App\Domains\Auth\Requests\RegisterRequest;
 use App\Services\Auth\Features\RegisterFeature;
 use Illuminate\Http\Response;
 use Lucid\Units\Controller;
+use OpenApi\Annotations as OA;
 
+/**
+ * @OA\Post(
+ *     path="/register",
+ *     tags={"auth"},
+ *     summary="register user with email and password",
+ *     @OA\RequestBody(
+ *         required=true,
+ *         @OA\JsonContent(
+ *             @OA\Property(property="first_name", type="string", description="first name", nullable=true),
+ *             @OA\Property(property="last_name", type="string", description="last name", nullable=true),
+ *             @OA\Property(property="email", type="string", format="email", description="User's email"),
+ *             @OA\Property(property="password", type="string", format="password", description="User's password"),
+ *
+ *         )
+ *     ),
+ *     @OA\Response(
+ *         response=200,
+ *         description="Successful register new user"
+ *     ),
+ *     @OA\Response(
+ *         response=422,
+ *         description="Unprocessable - Invalid credentials"
+ *     )
+ * )
+ */
 class RegisterController extends Controller
 {
     public function __construct(
